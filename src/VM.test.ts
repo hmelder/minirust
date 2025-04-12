47e2ef12d6d3258eb25f29a0b92573650c27d206
hmelder <service@hugomelder.com>
2025-04-13 00:24:47 +0800

    Add a VM test

---
+++ src/VM.test.ts
@@ -0,0 +1,36 @@
+import { test } from 'node:test'
+import assert from 'node:assert/strict'
+import { VM } from './VM'
+
+test('VM', async (t) => {
+    await t.test('should return last item on stack as return value', () => {
+        assert.deepStrictEqual(
+            VM.execute([{ opcode: 'PUSH', value: 42 }, { opcode: 'HALT' }]),
+            [42]
+        )
+    })
+    await t.test('should execute add binop', () => {
+        assert.deepStrictEqual(
+            VM.execute([
+                { opcode: 'PUSH', value: 1 },
+                { opcode: 'PUSH', value: 2 },
+                { opcode: 'ADD' },
+                { opcode: 'HALT' },
+            ]),
+            [3]
+        )
+    })
+    await t.test('should execute chained add binop', () => {
+        assert.deepStrictEqual(
+            VM.execute([
+                { opcode: 'PUSH', value: 1 },
+                { opcode: 'PUSH', value: 2 },
+                { opcode: 'ADD' },
+                { opcode: 'PUSH', value: 3 },
+                { opcode: 'ADD' },
+                { opcode: 'HALT' },
+            ]),
+            [6]
+        )
+    })
+})
