bd890508b5240e22f17a098cdf11ab4d37d2216a
hmelder <service@hugomelder.com>
2025-04-14 12:17:11 +0800

    ConductorBridge: Use driver's evaluate function

--- src/ConductorBridge.ts
+++ src/ConductorBridge.ts
@@ -1,15 +1,7 @@
 import { BasicEvaluator } from 'conductor/dist/conductor/runner'
 import { IRunnerPlugin } from 'conductor/dist/conductor/runner/types'
 
-import { MiniRustLexer } from './parser/src/MiniRustLexer'
-import { MiniRustParser } from './parser/src/MiniRustParser'
-import { CharStream, CommonTokenStream } from 'antlr4ng'
-
-import { TypeChecker } from './TypeChecker'
-import { MIRLowering } from './MIRLowering'
-import { MIRToVMLowering } from './MIRToVMLowering'
-import { VM } from './VM'
-import { MIR } from './MIR'
+import { evaluate } from './Driver'
 
 export class ConductorBridge extends BasicEvaluator {
     private executionCount: number
@@ -24,30 +16,7 @@ export class ConductorBridge extends BasicEvaluator {
     async evaluateChunk(chunk: string): Promise<void> {
         this.executionCount++
         try {
-            // Create the lexer and parser
-            const inputStream = CharStream.fromString(chunk)
-            const lexer = new MiniRustLexer(inputStream)
-            const tokenStream = new CommonTokenStream(lexer)
-            const parser = new MiniRustParser(tokenStream)
-            const checker = new TypeChecker()
-
-            // Parse the input
-            const tree = parser.prog()
-
-            const typeErrors = checker.check(tree)
-            if (typeErrors.length !== 0) {
-                throw new Error(`Encountered type error(s): ${typeErrors}`)
-            }
-
-            // TODO: Check if this actually returns a graph
-            const graph = this.visitor.visit(tree) as MIR.Graph
-            const vmLowering = new MIRToVMLowering(graph)
-            const instrs = vmLowering.lowerFunction()
-            const vm = new VM.Executor(instrs)
-            const resultSlot = vm.run()
-
-            const result = resultSlot.value
-
+            const result = evaluate(chunk)
             // Send the result to the REPL
             this.conductor.sendOutput(`Result of expression: ${result}`)
         } catch (error) {
